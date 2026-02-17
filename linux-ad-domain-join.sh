bdbcf128ddf34af0b6dde705311c4be6251ec601
Lucas Bonfim de Oliveira Lima <soulucasbonfim@gmail.com>
2026-02-17 13:25:04 -0300

    fix: version

--- linux-ad-domain-join.sh
+++ linux-ad-domain-join.sh
@@ -6,7 +6,7 @@
 # LinkedIn:    https://www.linkedin.com/in/soulucasbonfim
 # GitHub:      https://github.com/soulucasbonfim
 # Created:     2025-04-27
-# Version:     3.2.6
+# Version:     3.2.7
 # License:     MIT
 # -------------------------------------------------------------------------------------------------
 # Description:
